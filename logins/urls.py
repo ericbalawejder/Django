9943ddd7a5e68479e500a79557f683d96d1661ab
Eric Balawejder <eric.balawejder@gmail.com>
2016-06-22 17:40:44 -0400

    created details hyperlinks

--- logins/urls.py
+++ logins/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from . import views   # or: from logins import views
  
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     
-    url(r'^by_ip/$', views.by_ip),
-    url(r'^by_login/$', views.by_login),
+    url(r'^by_ip/$', views.by_ip, name='by_ip'),
+    url(r'^(?P<fail_id>\d+)/$', views.details),
+    url(r'^by_login/$', views.by_login, name='by_login'),
 ]
  
--- logins/views.py
+++ logins/views.py
@@ -2,15 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
+from logins.models import Fail
  
 def index(request):
-    #return HttpResponse("The logins index.")
-    return render(request, 'index.html')
+    #fail = Fail.objects.filter(visible=True)
+    failed = Fail.objects.all()
+    print failed
+    context = {'failed': failed}
+    return render(request, 'index.html', context)
 
 def by_ip(request):
     #return HttpResponse("The logins index.")
-    return render(request, 'by_ip.html')
+    failed = Fail.objects.all()
+    print failed
+    context = {'failed': failed}
+    return render(request, 'by_ip.html', context)
 
 def by_login(request):
     #return HttpResponse("The logins index.")
-    return render(request, 'by_login.html')
+    failed = Fail.objects.all()
+    print failed
+    context = {'failed': failed}
+    return render(request, 'by_login.html', context)
+
+from django.shortcuts import get_object_or_404
+ 
+def details(request, fail_id):
+    fail = get_object_or_404(Fail, pk = fail_id)
+    return render(request, 'details.html', {'fail': fail})
+
+
